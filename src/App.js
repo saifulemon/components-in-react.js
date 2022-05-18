da297e48cd28c7c29841c946b8df8f45911993a6
saifulemon <463saiful0@gmail.com>
2022-05-18 11:10:35 +0600

    multiple dynamic data pass

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
+  const products = [
+    {name: 'Photoshop', price: '$99.99'},
+    {name: 'Illustrator', price: '$65.21'},
+    {name: 'PDF Reader', price: '$6.00'}
+  ];
   return (
     <div className="App">
       <header className="App-header">
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+
         <Person name="Tamim Iqbal" status="Batesman"></Person>
         <Person name="Mushfiqur Rahim" status="Batesman & wicket kipper"></Person>
         <Person name="Mostafizur Rahman" status="Boller"></Person>
       </header>
     </div>
   );
+}
+
+function Product (props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: 'auto',
+    width: '200px',
+    float: 'left'
+  }
+
+  const {name, price} = props.product;
+  return (
+    <div style={productStyle}>
+      <h2>{name}</h2>
+      <h1>{price}</h1>
+      <button>Buy now</button>
+    </div>
+  )
 }
 
 function Person (props) {
